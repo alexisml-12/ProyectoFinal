60a0a2f9a5b57aa2f1d1bcfbfaefe19aa98b1b26
Alexis <amurillo82943@misena.edu.co>
2020-11-01 13:43:00 -0500

    conexion a la BD

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect (
+    'localhost',
+    'root',
+    '',
+    'db_proyectofinal'
+);
+
+// if (isset($conn)){
+//     echo 'esta conectada';
+// }
+
+?>
