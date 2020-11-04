a1b7ec28547d9ef3409a051fe1257fa97f4dde9e
Alexis <amurillo82943@misena.edu.co>
2020-11-03 20:35:16 -0500

    Pagina Principal

--- index.php
+++ index.php
@@ -7,15 +7,28 @@
     <title>Inicio</title>
 </head>
 <body>
-<nav class="navbar navbar-dark bg-primary">
-  <a class="navbar-brand" href="">AD Conjuntos Residenciales</a>
-  <a class="nav-link" href="#">Features</a>
-  <form class="form-inline">
-    <a class="navbar-brand" href="#">Iniciar Sesion</a>
-  </form>
-</nav>
-
-
+    <nav class="navbar navbar-dark bg-primary">
+        <a class="navbar-brand" href="">AD Conjuntos Residenciales</a>
+        <a class="navbar-brand" href="#">Acerca de nosotros</a>
+        <a class="navbar-brand" href="#">Contacto</a>
+        <a class="navbar-brand" href="#">Soporte</a>
+        <form class="form-inline">
+            <a class="navbar-brand" href="#">Iniciar Sesion</a>
+        </form>
+    </nav>
+<div class="container">
+    <div style="background:transparent !important" class="jumbotron">
+        <center><h1>La mejor manera de administrar tu edificio</h1></center>
+        <br>
+        <p class="lead">AD Conjuntos residenciales es una aplicacion desarrollada para brindar el mejor
+        servicio para el manejo de informacion de unidades residenciales que tambien cuenta con una interfaz
+        para los inquilinos, propietarios y trabajadores de la unidad.</p>
+        <br>
+        <div class="text-center">
+            <img src="https://www.metrocuadrado.com/noticias/sites/default/files/styles/full_image/public/field/image/68678231_ml.jpg?itok=0ib6VkoJ" class="rounded">
+        </div>
+    </div>
+</div> 
 
 
     <script src="js/jquery-3.5.1.min.js"></script>
