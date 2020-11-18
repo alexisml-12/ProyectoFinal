57d21d2fe2f6906b45e63b3adf9b306e19b07a12
Daniel Puerta <otrascosasdaniel061116@gmail.com>
2020-11-18 18:49:51 -0500

    gds

---
+++ login/Administrador/infcontable.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+    <title>Información contable</title>
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" href="">AD Conjuntos Residenciales</a>
+  <form class="form-inline">
+    <a class="navbar-brand" href="">Soporte</a>
+    <a class="navbar-brand" href="logoutadmn.php">Cerrar sesion</a>
+  </form>
+</nav>
+    
+</body>
+</html>
