8fd28c3a3c28d2aee9c47b0d2cf2537aa48a1aa1
Alexis <amurillo82943@misena.edu.co>
2020-12-04 13:46:30 -0500

    yep

--- login/Administrador/interfazAdm.php
+++ login/Administrador/interfazAdm.php
@@ -82,13 +82,13 @@ if (!isset($usuario)) {
     </ul>
 </div>
 <br><br><br><br><br><br>
-<div class="container">
+<!-- <div class="container">
 <ul style="float:left;">
     <li>
       <a style="color:#1C2833;" href="infcontable.php"><h5>Consultar informacion contable<span class="icon-down-open"></h5></a></span>   
     </li>
     </ul>
-</div>
+</div> -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
